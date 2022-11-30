aed1215b0a51649cc7d4c8a8218f6c63b061ba0c
Makconia <makconia@gmail.com>
2022-11-30 22:40:59 +0300

    Правки

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -8,18 +8,18 @@ Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1>number2 && number1>number3)
+if (number1 > number2 && number1 > number3)
     {
         Console.WriteLine("Наибольшее число: ");
         Console.WriteLine(number1);
     }
-else if (number2>number3)
+else if (number2 > number3)
     {
         Console.WriteLine("Наибольшее число: ");
         Console.WriteLine(number2);
     }
 else
         {
-            Console.WriteLine("Наибольшее число: ");
+        Console.WriteLine("Наибольшее число: ");
         Console.WriteLine(number3);
         }
